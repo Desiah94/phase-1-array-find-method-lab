8ba1b26b1bb163cecff84caed0dfd22e2b438dad
Desiah Barnett <desiahbarnett@gmail.com>
2024-01-10 16:13:13 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+function superbowlWin(record) {
+    // Use the find() method to search for a win in the record array
+    const win = record.find(event => event.result === "W");
+  
+    // If a win is found, return the corresponding year
+    if (win) {
+      return win.year;
+    }
+  
+    // If no win is found, return undefined
+    return undefined;
+  }
+  
